07130f2f95d1395ace5b7398e255b61c731d026e
Shuhan Wang <shuhanw2@illinois.edu>
2020-07-23 16:47:11 -0400

    update App.js

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { View, Dimensions} from 'react-native';
+import { View, Dimensions, Text } from 'react-native';
 import Gradient from './components/RadialGradient';
+import WaveContainer from './components/WaveContainer';
+import StatusText from './components/StatusText';
+import { Svg, G } from 'react-native-svg';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const HelloWorldApp = () => {
     return (
-        <View style={
-            {
-                flex: 1,
-                backgroundColor: '#8a939a'
-            }
-        }>
-            <Gradient x={windowWidth / 10} y={windowHeight / 2} r={windowWidth} incolor='#b9c1c9' outcolor='#8a939a'></Gradient>
+        <View style={{backgroundColor: '#8a939a', flex: 1}}>
+
+            <View style={{position: 'absolute'}}>
+                <Gradient x={windowWidth / 10} y={windowHeight / 2} r={windowWidth} incolor='#b9c1c9' outcolor='#8a939a'></Gradient>
+            </View>
+
+            <View style={{position: 'absolute'}}>
+                <WaveContainer></WaveContainer>
+            </View>
+            
+            <View style={{position: 'absolute'}}>
+                <StatusText></StatusText>
+            </View>
+
         </View>
     )
 }
