e4e23a2c8f6c0e7ab8b0ab97023648efb58b41ff
Shuhan Wang <shuhanw2@illinois.edu>
2020-07-23 16:46:53 -0400

    resize outer circle

--- components/WaveContainer.js
+++ components/WaveContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Dimensions } from 'react-native';
 import Gradient from './RadialGradient'
+import { Svg, Circle } from 'react-native-svg';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -8,7 +9,7 @@ const windowHeight = Dimensions.get('window').height;
 const WaveContainer = () => {
     return (
         <View>
-            <Gradient x={windowWidth / 2} y={windowHeight * 0.382} r={windowWidth * 0.618 / 1.5} incolor='#c2c6cc' outcolor='#a6a9bf'></Gradient>
+            <Gradient x={windowWidth / 2} y={windowHeight / 3} r={windowWidth * 0.618 / 1.7} incolor='#c2c6cc' outcolor='#a6a9bf'></Gradient>
         </View>
     )
 }
