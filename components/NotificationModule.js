13a2019a08f49383a4127710fb4881ebf635b558
Shuhan Wang <shuhanw2@illinois.edu>
2020-07-26 19:00:09 -0400

    add remaining time state to notification module

--- components/NotificationModule.js
+++ components/NotificationModule.js
@@ -1,69 +1,84 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Dimensions, View, StyleSheet, Text, Switch } from 'react-native';
 import { Timer, FlipNumber } from 'react-native-flip-timer';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const NotificationModule = () => {
+class NotificationModule extends Component {
 
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    constructor(props) {
+        super(props);
+    }
 
-    return (
-        <View style={_style.RoundedRect}>
-            <Text style={_style.GrayText}>杀菌净味完成提醒</Text>
-            <View style={_style.GrayRect}>
-                <Text style={{ position: 'absolute', top: 15, left: 17.5, fontSize: 14 }}>距净味完成还有</Text>
-                <Text style={{ position: 'absolute', bottom: 15, left: 52.5, fontSize: 14 }}>分钟</Text>
+    state = {
+        isEnabled: false,
+        odorTime: 27,
+        sterTime: 3684
+    };
 
-                <Text style={{ position: 'absolute', top: 15, right: 60, fontSize: 14 }}>距杀菌完成还有</Text>
-                <Text style={{ position: 'absolute', bottom: 15, right: 52.5, fontSize: 14 }}>分钟</Text>
-                <Text style={{ position: 'absolute', bottom: 15, right: 142.5, fontSize: 14 }}>小时</Text>
+    toggleSwitch = () => {
+        this.setState({
+            isEnabled: !this.state.isEnabled
+        })
+    }
 
-                <View style={[_style.TimeCard, { left: 30 }]}>
-                    <Text style={_style.TimeText}>
-                        0
+    render() {
+        return (
+            <View style={_style.RoundedRect}>
+                <Text style={_style.GrayText}>杀菌净味完成提醒</Text>
+                <View style={_style.GrayRect}>
+                    <Text style={{ position: 'absolute', top: 15, left: 17.5, fontSize: 14 }}>距净味完成还有</Text>
+                    <Text style={{ position: 'absolute', bottom: 15, left: 52.5, fontSize: 14 }}>分钟</Text>
+
+                    <Text style={{ position: 'absolute', top: 15, right: 60, fontSize: 14 }}>距杀菌完成还有</Text>
+                    <Text style={{ position: 'absolute', bottom: 15, right: 52.5, fontSize: 14 }}>分钟</Text>
+                    <Text style={{ position: 'absolute', bottom: 15, right: 142.5, fontSize: 14 }}>小时</Text>
+
+                    <View style={[_style.TimeCard, { left: 30 }]}>
+                        <Text style={_style.TimeText}>
+                            {Math.floor(this.state.odorTime / 10)}
+                        </Text>
+                    </View>
+                    <View style={[_style.TimeCard, { left: 70 }]}>
+                        <Text style={_style.TimeText}>
+                            {this.state.odorTime % 10}
+                        </Text>
+                    </View>
+
+                    <View style={[_style.TimeCard, { right: 160 }]}>
+                        <Text style={_style.TimeText}>
+                            {Math.floor(this.state.sterTime / 60 / 10)}
+                        </Text>
+                    </View>
+                    <View style={[_style.TimeCard, { right: 120 }]}>
+                        <Text style={_style.TimeText}>
+                            {Math.floor((this.state.sterTime / 60) % 10)}
+                        </Text>
+                    </View>
+
+                    <View style={[_style.TimeCard, { right: 70 }]}>
+                        <Text style={_style.TimeText}>
+                            {Math.floor((this.state.sterTime % 60) / 10)}
+                        </Text>
+                    </View>
+                    <View style={[_style.TimeCard, { right: 30 }]}>
+                        <Text style={_style.TimeText}>
+                            {(this.state.sterTime % 60) % 10}
                     </Text>
-                </View>
-                <View style={[_style.TimeCard, { left: 70 }]}>
-                    <Text style={_style.TimeText}>
-                        7
-                    </Text>
-                </View>
+                    </View>
 
-                <View style={[_style.TimeCard, { right: 160 }]}>
-                    <Text style={_style.TimeText}>
-                        0
-                    </Text>
+                    <Text style={[_style.TimeText, { right: 107.5 }]}>:</Text>
                 </View>
-                <View style={[_style.TimeCard, { right: 120 }]}>
-                    <Text style={_style.TimeText}>
-                        1
-                    </Text>
-                </View>
-
-                <View style={[_style.TimeCard, { right: 70 }]}>
-                    <Text style={_style.TimeText}>
-                        4
-                    </Text>
-                </View>
-                <View style={[_style.TimeCard, { right: 30 }]}>
-                    <Text style={_style.TimeText}>
-                        8
-                    </Text>
-                </View>
-
-                <Text style={[_style.TimeText, { right: 107.5 }]}>:</Text>
+                <Switch
+                    style={_style.GreenSwitch}
+                    trackColor={{ false: "#ededed", true: "#448dd1" }}
+                    ios_backgroundColor="#ededed"
+                    onValueChange={this.toggleSwitch}
+                    value={this.state.isEnabled}></Switch>
             </View>
-            <Switch
-                style={_style.GreenSwitch}
-                trackColor={{ false: "#ededed", true: "#448dd1" }}
-                ios_backgroundColor="#ededed"
-                onValueChange={toggleSwitch}
-                value={isEnabled}></Switch>
-        </View>
-    )
+        )
+    }
 }
 
 const _style = StyleSheet.create({
