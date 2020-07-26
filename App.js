abd1ecb277fd416f26759189320eec6dad850be5
Shuhan Wang <shuhanw2@illinois.edu>
2020-07-26 19:27:25 -0400

    add navigation between home page and introduction page

--- App.js
+++ App.js
@@ -1,35 +1,76 @@
 import React from 'react';
-import { View, Dimensions, Text } from 'react-native';
+import { View, Dimensions, Button, } from 'react-native';
 import Gradient from './components/RadialGradient';
 import WaveContainer from './components/WaveContainer';
 import StatusText from './components/StatusText';
 import NotificationModule from './components/NotificationModule';
-import { Svg, G } from 'react-native-svg';
+import 'react-native-gesture-handler';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const HelloWorldApp = () => {
+const MeijuApp = () => {
     return (
-        <View style={{backgroundColor: '#8a939a', flex: 1}}>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='home' component={HomeScreen} options={({ navigation, route }) => ({
+                    title: '您的冰箱: BCD-468WDNMD',
+                    headerTransparent: true,
+                    headerTintColor: 'white',
+                    headerRight: () => (
+                        <Button title='?' onPress={() => navigation.navigate('introduction')} color='white'></Button>
+                    )
+                })}>
 
-            <View style={{position: 'absolute'}}>
+                </Stack.Screen>
+                <Stack.Screen name='introduction' component={IntroductionScreen} options={{
+                    title: '美的PST+超磁电离净味',
+                    headerTransparent: true,
+                    headerTintColor: 'white',
+                }}>
+
+                </Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+const HomeScreen = () => {
+    return (
+        <View style={{ backgroundColor: '#8a939a', flex: 1 }}>
+
+            <View style={{ position: 'absolute' }}>
                 <Gradient x={windowWidth / 10} y={windowHeight / 2} r={windowWidth} incolor='#b9c1c9' outcolor='#8a939a'></Gradient>
             </View>
 
-            <View style={{position: 'absolute'}}>
+            <View style={{ position: 'absolute' }}>
                 <WaveContainer></WaveContainer>
             </View>
 
-            <View style={{position: 'absolute'}}>
+            <View style={{ position: 'absolute' }}>
                 <StatusText></StatusText>
             </View>
 
-            <View style={{position: 'absolute'}}>
+            <View style={{ position: 'absolute' }}>
                 <NotificationModule></NotificationModule>
             </View>
 
         </View>
     )
 }
-export default HelloWorldApp;
+
+const IntroductionScreen = () => {
+    return (
+        <View style={{ backgroundColor: '#8a939a', flex: 1 }}>
+
+            <View style={{ position: 'absolute' }}>
+                <Gradient x={windowWidth / 10} y={windowHeight / 2} r={windowWidth} incolor='#b9c1c9' outcolor='#8a939a'></Gradient>
+            </View>
+        </View>
+    )
+}
+
+export default MeijuApp;
