78267e4bc41f6ddcad5b9df30d5ddd1f27abc4b8
Shuhan Wang <shuhanw2@illinois.edu>
2020-07-26 18:45:40 -0400

    finish static notification module

--- components/NotificationModule.js
+++ components/NotificationModule.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Dimensions, View, StyleSheet, Text, Switch } from 'react-native';
 import { Timer, FlipNumber } from 'react-native-flip-timer';
 
@@ -14,10 +14,50 @@ const NotificationModule = () => {
         <View style={_style.RoundedRect}>
             <Text style={_style.GrayText}>杀菌净味完成提醒</Text>
             <View style={_style.GrayRect}>
-                
+                <Text style={{ position: 'absolute', top: 15, left: 17.5, fontSize: 14 }}>距净味完成还有</Text>
+                <Text style={{ position: 'absolute', bottom: 15, left: 52.5, fontSize: 14 }}>分钟</Text>
+
+                <Text style={{ position: 'absolute', top: 15, right: 60, fontSize: 14 }}>距杀菌完成还有</Text>
+                <Text style={{ position: 'absolute', bottom: 15, right: 52.5, fontSize: 14 }}>分钟</Text>
+                <Text style={{ position: 'absolute', bottom: 15, right: 142.5, fontSize: 14 }}>小时</Text>
+
+                <View style={[_style.TimeCard, { left: 30 }]}>
+                    <Text style={_style.TimeText}>
+                        0
+                    </Text>
+                </View>
+                <View style={[_style.TimeCard, { left: 70 }]}>
+                    <Text style={_style.TimeText}>
+                        7
+                    </Text>
+                </View>
+
+                <View style={[_style.TimeCard, { right: 160 }]}>
+                    <Text style={_style.TimeText}>
+                        0
+                    </Text>
+                </View>
+                <View style={[_style.TimeCard, { right: 120 }]}>
+                    <Text style={_style.TimeText}>
+                        1
+                    </Text>
+                </View>
+
+                <View style={[_style.TimeCard, { right: 70 }]}>
+                    <Text style={_style.TimeText}>
+                        4
+                    </Text>
+                </View>
+                <View style={[_style.TimeCard, { right: 30 }]}>
+                    <Text style={_style.TimeText}>
+                        8
+                    </Text>
+                </View>
+
+                <Text style={[_style.TimeText, { right: 107.5 }]}>:</Text>
             </View>
-            <Switch 
-                style={_style.GreenSwitch} 
+            <Switch
+                style={_style.GreenSwitch}
                 trackColor={{ false: "#ededed", true: "#448dd1" }}
                 ios_backgroundColor="#ededed"
                 onValueChange={toggleSwitch}
@@ -36,28 +76,46 @@ const _style = StyleSheet.create({
         width: windowWidth - 40,
         backgroundColor: '#ffffff',
         borderRadius: 20,
-        shadowOffset:{  width: 1,  height: 1,  },
+        shadowOffset: { width: 1, height: 1, },
         shadowColor: 'black',
         shadowOpacity: 0.5,
     },
     GrayText: {
-        top: windowHeight * 0.05 - 10 - 9,
+        top: 16,
         left: 20,
         fontSize: 18,
-        fontWeight: 'bold'
     },
     GrayRect: {
         position: 'absolute',
         alignSelf: 'center',
-        top: windowHeight * 0.1 - 20,
-        height: windowHeight * 0.17,
+        justifyContent: 'center',
+        top: 50,
+        height: windowHeight * 0.27 - 70,
         width: windowWidth - 40,
         backgroundColor: '#ededed',
     },
     GreenSwitch: {
         position: 'absolute',
-        top: 8,
+        top: 10,
         right: 10,
+    },
+    TimeCard: {
+        position: 'absolute',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        top: (windowHeight * 0.27 - 70) * 0.3,
+        height: (windowHeight * 0.27 - 70) * 0.4,
+        width: 35,
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        shadowOffset: { width: 0.5, height: 1, },
+        shadowColor: 'black',
+        shadowOpacity: 0.3,
+    },
+    TimeText: {
+        position: 'absolute',
+        alignSelf: 'center',
+        fontSize: (windowHeight * 0.27 - 70) * 0.2,
     }
 })
 
